9d5558dce9201c20056cd023c127a2cd61536876
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-06-11 23:11:13 +0200

    ADD style components: CategoryCard and CategoryList

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,15 +1,21 @@
-import CategoryCard from "./CategoryCard.jsx";
+import CategoryCard from './CategoryCard.jsx';
 
 function CategoryList({ categories }) {
     return (
-        <div>
-            {categories.map((category) => (
-                <CategoryCard key={category.id} category={category} />
-            ))}
-        </div>
+        <>
+            <h1 className='fw-bold'>Le Nostre Dinastie</h1>
+            <p className=' text-body-secondary'>Ogni dinastia ha il suo sacro carattere!!</p>
+            <div className='container py-5'>
+                <div className='row g-4'>
+                    {categories.map((category) => (
+                        <div key={category.id} className='col-12 col-md-6 col-lg-4'>
+                            <CategoryCard category={category} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
 
 export default CategoryList;
-
-
